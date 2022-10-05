a3538b19705791e2c48dfab25b8a9a2f76ec5524
Mohammed Alostah <mohammed.eloustah@tarjama.com>
2022-10-05 14:34:45 +0300

    a

--- src/Commands/Compo.php
+++ src/Commands/Compo.php
@@ -5,6 +5,7 @@ namespace Ostah\LaravelCompo\Commands;
 
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\File;
 
 class Compo extends Command
 {
@@ -13,24 +14,15 @@ class Compo extends Command
      *
      * @var string
      */
-    protected $signature = 'comp:service';
+    protected $signature = 'comp:install';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Create a new Service for spacific controller';
+    protected $description = 'Install the Compo Package';
 
-    /**
-     * Create a new command instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        parent::__construct();
-    }
 
     /**
      * Execute the console command.
@@ -39,6 +31,48 @@ class Compo extends Command
      */
     public function handle()
     {
-        return 0;
+        $this->info('Installing Compo Package...');
+        $this->info('Publishing configuration...');
+
+        if (! $this->configExists('laravel-compo.php')) {
+            $this->publishConfiguration();
+            $this->info('Published configuration');
+        } else {
+            if ($this->shouldOverwriteConfig()) {
+                $this->info('Overwriting configuration file...');
+                $this->publishConfiguration($force = true);
+            } else {
+                $this->info('Existing configuration was not overwritten');
+            }
+        }
+
+        $this->info('Installed Compo Package');
     }
+    private function configExists($fileName)
+    {
+        return File::exists(config_path($fileName));
+    }
+
+    private function shouldOverwriteConfig()
+    {
+        return $this->confirm(
+            'Config file already exists. Do you want to overwrite it?',
+            false
+        );
+    }
+
+    private function publishConfiguration($forcePublish = false)
+    {
+        $params = [
+            '--provider' => "Ostah\LaravelCompo\LaravelCompoServiceProvider",
+            '--tag' => "config"
+        ];
+
+        if ($forcePublish === true) {
+            $params['--force'] = true;
+        }
+
+        $this->call('vendor:publish', $params);
+    }
+
 }
